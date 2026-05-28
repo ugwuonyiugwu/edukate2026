65c6837ced798f693cf87f62bbfdcd4c500c5edd
ugwuonyiugwu <assistfuto@gmail.com>
2026-05-28 14:32:47 +0100

    updated

--- src/app/(homepage)/quizathon/page.tsx
+++ src/app/(homepage)/quizathon/page.tsx
@@ -4,6 +4,8 @@ import { trpc, HydrateClient } from "@/trpc/server";
 import { QuizContent } from '@/modules/home/Quizathon/quizcontent';
 import { QuizSkeleton } from '@/modules/home/Quizathon/quizskeleton';
 
+export const dynamic = 'force-dynamic';
+
 export default async function MonthlyRegistrationPage() {
   void trpc.userquizathon.getLatestEvent.prefetch();
 
